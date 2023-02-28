016426ce177f43c898fc7c652d6b7c8f61c4001d
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-02-28 12:39:34 +0530

    githublogin

--- routes/github.oauth.route.js
+++ routes/github.oauth.route.js
@@ -4,10 +4,33 @@ const { UserModel } = require("../model/user.model");
 const githublogin = express.Router();
 const GitHubStrategy = require("passport-github2").Strategy;
 const session = require("express-session");
-const { sendEmail } = require("../nodemailer/sendingEmails");
 const bcrypt = require("bcrypt");
 // nodemailer
+const nodemailer = require("nodemailer");
 
+function sendEmail(email,credentials,name) {
+  //   console.log(email);
+  //   transpoter to send/transport email
+
+ const transporter = nodemailer.createTransport({
+   host: 'smtp.gmail.com',
+   port: 587,
+   auth: {
+     user: 'fsociety430@gmail.com',
+     pass: process.env.GoogleKey
+ }
+ });
+
+ transporter.sendMail({
+   to: `${email}`,
+   from: 'chikkuuu@gmail.com',
+   subject: 'WorkDesk Login credentials',
+   html: ` Hey, ${name} \n Thanks you for signUp \n \n Here is your email ${email} and Password : ${credentials}`
+ })
+ .then(()=>console.log('mail sent successfully'))
+ .catch((err)=>console.log(err.message))
+
+}
 
 //! post login route  given from github developer settings
 
